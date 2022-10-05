06885aa9a0798f05c7888be34a5b4c4858bdcfb2
EC2 Default User <ec2-user@ip-172-31-35-65.ap-northeast-3.compute.internal>
2022-10-05 14:21:31 +0000

    continue[Nagano_CAKE]

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to:'public/homes#top'
-  get'about' => 'public/homes/about'
+  get'about' => 'public/homes#about'
+  get'customers/my_page' => 'public/customers#show',as: :customer
+  get'customers/information/edit' => 'public/customers#edit',as: :edit_customer
+  patch 'customers/information' => 'public/customers#edit',as: :update_customer
+  get'customers/unsubscribe' => 'public/customers#subscribe',as: :subscribe_customer
+  patch 'customers/withdrawal' => 'public/customer#withdrawal',as: :withdrawal_customer
   namespace :admin do
     resources :genres,only: [:index,:edit,:create,:update]
     resources :items,only: [:index,:edit,:new,:create,:update]
