1670f9490eccea6edbb44bc08e08ec692227fd3f
EC2 Default User <ec2-user@ip-172-31-35-65.ap-northeast-3.compute.internal>
2022-10-10 15:15:15 +0000

    continue[Nagano_CAKE]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,6 +2,7 @@ class Public::CartItemsController < ApplicationController
   
   def index
     @cart_items = Cart_item.all
+    @cart_item.item = Item.find(params[:id])
   end
   
   def update
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_142523) do
+ActiveRecord::Schema.define(version: 2022_10_10_145438) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,10 @@ ActiveRecord::Schema.define(version: 2022_10_07_142523) do
     t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "item_id"
+    t.integer "customer_id"
+    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -130,6 +134,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_142523) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "customers"
+  add_foreign_key "cart_items", "customers"
+  add_foreign_key "cart_items", "items"
   add_foreign_key "items", "genres"
   add_foreign_key "orders", "customers"
 end
